44cd892a09fc7dcec3e9d3853dc9cc58a9dba0d4
Michael Vivirito <mvivirito@icloud.com>
2022-07-09 08:00:54 -0700

    display table on visual page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, redirect, url_for, request
 
+#Pandas and Matplotlib
+import pandas as pd
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from os.path import join
+
+#other requirements
+import io
+
 # create the application object
 app = Flask(__name__)
+
+# create stroke data
+target = join(app.static_folder, 'data/stroke-data.csv')
+stroke_data = pd.read_csv(target)
+table_data = stroke_data.head(30)
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -22,5 +40,12 @@ def login():
 
 @app.route('/main')
 def main():
-    return render_template('main.html')  # render a template
+    return render_template('main.html') 
+
+
+@app.route('/visual', methods=["POST", "GET"])
+def visual():
+    return render_template('visual.html',
+                           PageTitle = "Visual",
+                           table=[table_data.to_html(classes='data', index = False)], titles= stroke_data.columns.values)
 
